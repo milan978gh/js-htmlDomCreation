955d0ef958472aa47ab1c720da122ff305065c7a
milan978gh <amilandev78@gmail.com>
2021-01-20 17:47:36 +0100

    first and only

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// window.onload = () => {
+// }
+const first = document.createElement('div');
+const h1 = document.createElement('h1');
+
+first.classList.add('first-div');
+h1.classList.add('haone');
+h1.textContent = 'hawkes';
+
+
+
+
+first.appendChild(h1);
+document.body.appendChild(first);
